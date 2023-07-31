2750be9c48a223e6ad33f5a9bdcc458a8fb3c67a
Théo Santiago Müller <theosmuller@gmail.com>
2023-07-31 18:58:08 -0300

    adding buttons

--- lib/recipedetails.dart
+++ lib/recipedetails.dart
@@ -13,26 +13,84 @@ class RecipeDetailsPage extends StatelessWidget {
         title: Text(recipe.autocompleteterm),
       ),
       body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Image.network(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.25,
+              child: Image.network(
                 recipe.image,
-                width: MediaQuery.of(context).size.width , // Set the desired width for the image
-                height: 200, // Set the desired height for the image
-                fit: BoxFit.cover, // Set the image fit mode
+                fit: BoxFit.cover,
               ),
-              // Display the recipe details here
-              Text('Country: ${recipe.country}'),
-              Text('Ingredients: ${recipe.country}'),
-              Text('Instructions: ${recipe.keyword}'),
-              // Add more details as needed
-            ],
-          ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: ElevatedButton.icon(
+                      onPressed: () {
+                        // Add onPressed logic for "Steps" button
+                      },
+                      icon: const Icon(
+                        Icons.play_arrow,
+                        size: 24,
+                      ),
+                      label: const Text(
+                        'Steps',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color.fromARGB(255, 255, 109, 65), // Golden yellow color
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 16), // Add some spacing between buttons
+                  Expanded(
+                    child: ElevatedButton.icon(
+                      onPressed: () {
+                        // Add onPressed logic for "Save as Favorite" button
+                      },
+                      icon: const Icon(
+                        Icons.star,
+                        size: 24,
+                      ),
+                      label: const Text(
+                        'Save as Favorite',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.orange, // Orange color
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Country: ${recipe.country}'),
+                  const SizedBox(height: 12), // Add some spacing between sections
+                  Text('Ingredients: ${recipe.keyword}'),
+                  const SizedBox(height: 12), // Add some spacing between sections
+                  Text('Instructions: ${recipe.country}'),
+                  // Add more details as needed
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
 }
+
